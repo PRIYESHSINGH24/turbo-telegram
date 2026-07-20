39a883962b7f0228233a9d338d1528de8d5b9c91
PRIYESHSINGH24 <PRIYESHSINGH571@GMAIL.COM>
2026-07-20 20:10:29 +0530

    starting code

---
+++ apps/customer-portal/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@repo/ui";
+
+function App() {
+  return (
+    <>
+      <Button onClick={() => alert("button clicked")} />
+      <h1>Complete Frontend from and deployment</h1>
+    </>
+
+  );
+}
+
+export default App;
